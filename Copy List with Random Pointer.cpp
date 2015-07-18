5780c35c02ff7c0665389fa4519033e58a990a20
kang2015 <kangyanli001@163.com>
2015-07-18 13:36:04 +0800

    Update Copy List with Random Pointer.cpp

--- Copy List with Random Pointer.cpp
+++ Copy List with Random Pointer.cpp
@@ -6,6 +6,7 @@
  *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
  * };
  */
+ //solution 1- with a unordered_map
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
@@ -26,6 +27,36 @@ public:
                 RandomListNode* tmp = it2->second;
                 it->second->random = tmp;
             }
+        }
+        return ret;
+    }
+};
+
+//solution 2 - without unorder_map, put new node just following old node
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        RandomListNode *p = head,*tmp=NULL;
+        while(p){
+            tmp = p->next;
+            p->next = new RandomListNode(p->label);
+            p->next->next = tmp;
+            p = tmp;
+        }
+        p = head;
+        while(p){
+            p->next->random = p->random == NULL?NULL:p->random->next;
+            p = p->next->next;
+        }
+        p = head;
+        RandomListNode *ret = NULL;
+        RandomListNode **t = &ret;
+        while(p){
+            tmp = p->next;
+            p->next = tmp->next;
+            *t = tmp;
+            t = &(tmp->next);
+            p = p->next;
         }
         return ret;
     }
