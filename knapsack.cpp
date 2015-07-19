55eeb8f0654224bd56a3bb88a41cd0f64a679ea4
kang2015 <kangyanli001@163.com>
2015-07-19 16:09:42 +0800

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -15,6 +15,7 @@ int knapsack01(vector<int> w,vector<int> v, int total){
 	}
 	return dp[n][total];
 }
+
 int knapsack01_optimized(vector<int> w,vector<int> v, int total){
 	int n = w.size();
 	vector<int> dp(total+1,0);
@@ -25,25 +26,38 @@ int knapsack01_optimized(vector<int> w,vector<int> v, int total){
 	}
 	return dp[total];
 }
-int knapsackComplete(vector<int> w,vector<int> v, int total){
-	return 0;
+
+int knapsackComplete_optimized(vector<int> w,vector<int> v, int total){
+	int n= w.size();
+	vector<int> dp(total+1,0);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=total;j++){
+			if(j>=w[i]){
+				dp[j] = max(dp[j-w[i]]+v[i],dp[j]);
+			}
+		}
+	}
+	return dp[total];
 }
+
 int main(){
 	vector<int> w;
-	w.push_back(1);
 	w.push_back(2);
 	w.push_back(3);
-	w.push_back(4);
+	w.push_back(3);
+	w.push_back(1);
 
 	vector<int> v;
-	v.push_back(8);
+	v.push_back(4);
+	v.push_back(6);
 	v.push_back(7);
-	v.push_back(8);
-	v.push_back(5);
-	int total = 6;
+	v.push_back(3);
+	int total = 7;
 	int ans = knapsack01(w,v,total);
 	cout << ans << endl;
 	ans = knapsack01_optimized(w,v,total);
 	cout <<ans << endl;
+	ans = knapsackComplete_optimized(w,v,total);
+	cout << ans <<endl;
 	return 0;
 }
