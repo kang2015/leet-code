e2d3e769a59cfed411fca3130c94e30018967104
kang2015 <kangyanli001@163.com>
2015-07-26 00:53:32 +0800

    Update Find Minimum in Rotated Sorted Array.java

--- Find Minimum in Rotated Sorted Array.java
+++ Find Minimum in Rotated Sorted Array.java
@@ -1,4 +1,5 @@
 //wangMeng's method of loop invariant
+//For array without duplicates
 public class Solution {
     public int findMin(int[] nums) {
         int left=0,right = nums.length - 1;
@@ -8,6 +9,24 @@ public class Solution {
                 left = mid+1;
             }else if(nums[mid] < nums[right]){
                 right = mid;
+            }
+        }
+        return nums[right];
+    }
+}
+
+//For array that may contain duplicates
+public class Solution {
+    public int findMin(int[] nums) {
+        int left=0,right = nums.length - 1;
+        while(left < right){
+            int mid = (left+right) >> 1;
+            if(nums[mid]>nums[right]){
+                left = mid+1;
+            }else if(nums[mid] <nums[right]){
+                right = mid;
+            }else{
+                right--;
             }
         }
         return nums[right];
