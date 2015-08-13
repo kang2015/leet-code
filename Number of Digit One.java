6c295b22683e3c81fcb8046994464ed8edf03874
kang2015 <kangyanli001@163.com>
2015-08-14 00:18:03 +0800

    Create Number of Digit One.java

---
+++ Number of Digit One.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int countDigitOne(int n) {
+        int iFactor = 1;
+        int iCount = 0;
+        int iLowerNum = 0;
+        int iCurrNum = 0;
+        int iHigherNum = 0;
+        while(n/iFactor>=10){
+            iLowerNum = n - (n/iFactor)*iFactor;
+            iCurrNum = n/iFactor%10;
+            iHigherNum = n/(iFactor*10);
+            switch(iCurrNum){
+                case 0:
+                    iCount += iHigherNum*iFactor;
+                    break;
+                case 1:
+                    iCount += iHigherNum*iFactor + iLowerNum +1;
+                    break;
+                default:
+                    iCount += (iHigherNum+1)*iFactor;
+                    break;
+            }
+            iFactor *= 10;
+        }
+        iCurrNum = n/iFactor;
+        if(iCurrNum<=0) return iCount;
+        iCount += iCurrNum==1?(n%iFactor+1):iFactor;
+        return iCount;
+    }
+}
