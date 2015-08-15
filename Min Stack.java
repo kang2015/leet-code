5639fe07ebfa06a324706c6729a57875e16958e2
kang2015 <kangyanli001@163.com>
2015-08-15 16:50:32 +0800

    Update Min Stack.java

--- Min Stack.java
+++ Min Stack.java
@@ -1,38 +1,3 @@
-// one stack.cpp
-class MinStack {
-public:
-    long min;
-    stack<long> sta;
-    void push(int x) {
-        if(sta.empty()){
-            sta.push(0L);
-            min=x;
-        }else{
-           sta.push(x-min);
-           if(x<min) min=x;
-        }
-    }
-    void pop() {
-        if(sta.empty()) return;
-        long pop1=sta.top();
-        sta.pop();
-        if(pop1<0)
-        min=min-pop1;
-    }
-
-    int top() {
-        long top=sta.top();
-        if(top>0)
-        return (int)(top+min);
-        else
-        return (int)min;
-    }
-
-    int getMin() {
-        return (int)min;
-    }
-};
-
 //two stack, one for data, one for current min
 class MinStack {
     private Stack<Integer> data = new Stack<Integer>();
